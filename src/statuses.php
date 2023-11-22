cd3e90db39efe15eb6626d9f67ba06f04e3831a6
Andrey Helldar <helldar@dragon-code.pro>
2023-11-23 02:49:25 +0300

    Fixed: Element ID is not unique

--- src/statuses.php
+++ src/statuses.php
@@ -43,11 +43,15 @@ function moveStatus(string $package, Locale $locale): void
 
     $title = LocaleNames::get($locale)[$locale->value];
 
+    $id = sprintf('statuses-%s-%s', $package, $locale->value);
+
     $content = file_get_contents($sourcePath);
 
     $content = str_replace('---' . PHP_EOL . 'editLink: false' . PHP_EOL . '---' . PHP_EOL . PHP_EOL, '', $content);
     $content = str_replace(PHP_EOL . '[ [go back](../status.md) | [to top](#) ]' . PHP_EOL . PHP_EOL, '', $content);
-    $content = preg_replace('/^#\s([a-zA-Z-_]+)/', '# ' . $title, $content);
+    $content = preg_replace('/^#\s([a-zA-Z-_]+)/m', "# $title\n{id=\"$id-title\"}", $content);
+    $content = preg_replace('/^###\s([\w-]+)\n?\r?$/m', "## \$1\n{id=\"$id-\$1\"}", $content);
+    $content = preg_replace('/^#####\s(.+):\s(\d+)/m', "> \$1: \$2\n>\n{style=\"warning\"}", $content);
 
     file_put_contents($targetPath . "statuses-$package-$locale->value.md", $content);
 }
