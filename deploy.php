1e1989322b320bf3baeeb08c6a11b656084b09ec
Andrey Helldar <helldar@dragon-code.pro>
2023-11-30 01:44:29 +0300

    Trying to fix the deployer

--- deploy.php
+++ deploy.php
@@ -23,7 +23,7 @@ set('rsync', [
     'timeout'       => 60,
 ]);
 
-set('rsync_src', __DIR__ . '/../dir');
+set('rsync_src', __DIR__ . '/dir');
 set('rsync_dest', '{{release_path}}');
 
 host('production')
