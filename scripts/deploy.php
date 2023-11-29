b1624734b8899540189ed9e814b7bcc82d7f91b4
Andrey Helldar <helldar@dragon-code.pro>
2023-11-29 22:45:42 +0300

    Added deployment script to the server

---
+++ scripts/deploy.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Deployer;
+
+require 'contrib/rsync.php';
+require 'recipe/common.php';
+
+set('rsync', [
+    'exclude'       => [
+        '.git',
+        'deploy.php',
+    ],
+    'exclude-file'  => false,
+    'include'       => [],
+    'include-file'  => false,
+    'filter'        => [],
+    'filter-file'   => false,
+    'filter-perdir' => false,
+    'flags'         => 'rz',
+    'options'       => ['delete'],
+    'timeout'       => 60,
+]);
+
+set('rsync_src', __DIR__ . '/../dir');
+set('rsync_dest', '{{release_path}}');
+
+host('production')
+    ->setHostname(getenv('DEPLOY_IP'))
+    ->setRemoteUser(getenv('DEPLOY_USERNAME'))
+    ->setDeployPath(getenv('DEPLOY_PATH'));
+
+task('deploy', [
+    'deploy:info',
+    'deploy:setup',
+    'deploy:lock',
+    'deploy:release',
+    'rsync',
+    'deploy:symlink',
+    'deploy:unlock',
+    'deploy:success',
+]);
+
+after('deploy:failed', 'deploy:unlock');
