fcd329b6ecff4297ead39e78bd6a168538e478a1
Andrey Helldar <helldar@dragon-code.pro>
2024-07-10 13:40:44 +0300

    Added information for Routes 1.5

--- docs/snippets/packages-routes-auto-locale.php
+++ docs/snippets/packages-routes-auto-locale.php
@@ -2,14 +2,17 @@
 
 declare(strict_types=1);
 
+localizedRoute('show', ['bar' => 'show']);
+// https://example.com/fr/foo/show
+
+localizedRoute('show', ['bar' => 'show', 'locale' => 'de']);
+// https://example.com/de/foo/show
+
+localizedRoute('localized.show', ['bar' => 'show']);
+// https://example.com/fr/foo/show
+
+localizedRoute('localized.show', ['bar' => 'show', 'locale' => 'de']);
+// https://example.com/de/foo/show
+
 route('localized.show', ['bar' => 'show', 'locale' => app()->getLocale()]);
 // https://example.com/fr/foo/show
-
-route('localized.show', ['bar' => 'show', 'locale' => 'de']);
-// https://example.com/de/foo/show
-
-route('localized.show', ['bar' => 'show']);
-// https://example.com/fr/foo/show
-
-route('localized.show', ['bar' => 'show', 'locale' => 'de']);
-// https://example.com/de/foo/show
